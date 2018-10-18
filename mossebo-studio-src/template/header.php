f32af4be1a2e84b0b3edec531e3a492a1c643401
Vlad Starkovsky <vlad@starkovsky.ru>
2018-10-18 16:03:38 +0300

    add favicons

--- mossebo-studio-src/template/header.php
+++ mossebo-studio-src/template/header.php
@@ -1,15 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <title><?php wp_title('|', true, 'right'); ?></title>
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="format-detection" content="telephone=no">
 
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500&amp;subset=cyrillic-ext" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo mix('assets/css/app.css') ?>">
+    <?php get_template_part( 'includes/meta' ) ?>
 
     <?php wp_head(); ?>
 
---
+++ public_html/wp-content/themes/mossebo/includes/meta.php
@@ -0,0 +1,23 @@
+
+<meta charset="<?php bloginfo('charset'); ?>">
+<title><?php wp_title('|', true, 'right'); ?></title>
+
+<meta name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="format-detection" content="telephone=no">
+
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500&amp;subset=cyrillic-ext" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo mix('assets/css/app.css') ?>">
+
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="apple-mobile-web-app-title" content="Mossebo.Studio">
+<meta name="application-name" content="Mossebo.Studio">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
