e02c45932976838990b11d80c25a2c63d33fbdbe
Vlad Starkovsky <vlad@starkovsky.ru>
2018-11-02 12:07:17 +0300

    fix

--- public_html/wp-content/themes/mossebo/page__thanks-franchise.php
+++ public_html/wp-content/themes/mossebo/page__thanks-franchise.php
@@ -72,44 +72,12 @@ get_header();
 
 </div>
 
-<div class="mt-48">
-    <div class="container">
-        <h2 class="title-h2">
-            <a class="instagram-title-link" href="<?php _the_field('instagram-mossebo') ?>" target="_blank" rel="nofollow noreferrer noopener">
-                <img
-                    src="<?= THEME_DIR ?>/assets/images/icons/socials/instagram.png"
-                    srcset="<?= THEME_DIR ?>/assets/images/icons/socials/instagram@2x.png 2x"
-                    alt="@mossebo.official"
-                >
-
-                @mossebo.official
-            </a>
-        </h2>
-
-        <div class="thanks-franchise-another-random-text">
-            <?php _e('Лучшие проекты Mossebo сначала выходят в нашем Instagram. Найди идеи для интерьера вместе с 300 000 наших подписчиков!') ?>
-        </div>
-
-        <u-block-instagram
-            profile="mossebo.official"
-            :count="8"
-        ></u-block-instagram>
-
-        <div class="watch-all-btn-wrap">
-            <a href="<?php _the_field('instagram-mossebo') ?>" class="watch-all-btn" target="_blank" rel="nofollow noreferrer noopener">
-                <span class="watch-all-btn__label">
-                    Смотреть все
-                </span>
-
-                <span class="watch-all-btn__icon-box">
-                    <svg class="watch-all-btn__icon">
-                        <use xlink:href="<?= THEME_DIR ?>/assets/images/icons.svg#symbol-arrow-forward"></use>
-                    </svg>
-                </span>
-            </a>
-        </div>
-    </div>
+<!-- Инстаграм общий -->
+<div class="mt-64">
+    <?php get_template_part( 'includes/instagram-last-8' ) ?>
 </div>
+<!-- Инстаграм общий конец -->
+
 
 <div class="mt-48">
     <div class="container">
